c97cb4e9598f24c4def86bcc0c47608e09f03319
Arunav Gupta <centaurus.ab@gmail.com>
2023-08-09 10:47:57 -0700

    update with new changes (#2)
    * Update recursive_feature_machine.py
    
    Device for eigenpro2 solver
    
    * Update recursive_feature_machine.py
    
    eigenpro now requires explicit specification using method='eigenpro'
    
    * Update recursive_feature_machine.py
    
    regularization added as a parameter to __init__()
    
    * Update recursive_feature_machine.py
    
    added a global flag EIGENPRO_AVAILABLE which is set if eigenpro is importable
    
    * batched M update step
    
    * fix a bug in update_M when M is diagonal
    
    * Update recursive_feature_machine.py
    
    Added GaussRFM
    
    * Update recursive_feature_machine.py
    
    * Update recursive_feature_machine.py
    
    added link to eigenpro2
    
    * Update __init__.py
    
    * Update recursive_feature_machine.py
    
    `torch.linalg.solve` warning
    
    * Update recursive_feature_machine.py
    
    * Update recursive_feature_machine.py
    
    * Update recursive_feature_machine.py
    
    testing eigenpro2
    
    * Update recursive_feature_machine.py
    
    * Update README.md
    
    * modified test and call to
    
    * refactor batched M update
    
    * remove self.diag check
    
    * rename `G` to `centers_term`
    
    * delete intermediate tensors
    
    * feat: automatic batching
    
    * remove verbosity
    
    * cleanup
    
    * cleanup
    
    * Update recursive_feature_machine.py
    
    * Update recursive_feature_machine.py
    
    initialized  `new_M` to `torch.zeros(d, d)` if `self.M is None`
    
    * Update recursive_feature_machine.py
    
    * Update recursive_feature_machine.py
    
    * mla in score, updated test_acc in fit (#5)
    
    * mla acc in score(), updated test_acc in fit()
    
    * updated accuracy computation
    
    * Update recursive_feature_machine.py
    
    changed import of `accuracy` function
    
    ---------
    
    Co-authored-by: Parthe Pandit <parthe@users.noreply.github.com>
    
    * Update recursive_feature_machine.py
    
    removed un-used `tqdm` and `trange` imports
    
    * fix BYTES_PER_SCALAR
    
    ---------
    
    Co-authored-by: Parthe Pandit <parthe@users.noreply.github.com>
    Co-authored-by: SeyedParsa <seyedparsamirtaheri@gmail.com>
    Co-authored-by: Parthe Pandit <parthe1292@gmail.com>
    Co-authored-by: Rohit Mishra <rohitnmishra2@gmail.com>

--- rfm/recursive_feature_machine.py
+++ rfm/recursive_feature_machine.py
@@ -1,20 +1,25 @@
 try:
-    from eigenpro2.models import KernelModel
-    from eigenpro3.utils import accuracy
+
+    from eigenpro2 import KernelModel
+    EIGENPRO_AVAILABLE = True
 except ModuleNotFoundError:
-    print(
-        "`eigenpro2` not installed.. using torch.linalg.solve for training kernel model"
-    )
+    print('`eigenpro2` is not installed...') 
+    print('Using `torch.linalg.solve` for training the kernel model\n')
+    print('WARNING: `torch.linalg.solve` scales poorly with the size of training dataset,\n '
+    '         and may cause an `Out-of-Memory` error')
+    print('`eigenpro2` is a more scalable solver. To use, pass `method="eigenpro"` to `model.fit()`')
+    print('To install `eigenpro2` visit https://github.com/EigenPro/EigenPro-pytorch/tree/pytorch/')
+    EIGENPRO_AVAILABLE = False
+    
 import torch, numpy as np
-from .kernels import laplacian_M, euclidean_distances_M
-from tqdm import tqdm, trange
+from torchmetrics.functional.classification import accuracy
+from .kernels import laplacian_M, gaussian_M, euclidean_distances_M
+from tqdm.contrib import tenumerate
 import hickle
 
 
 class RecursiveFeatureMachine(torch.nn.Module):
-    def __init__(
-        self, device=torch.device("cpu"), mem_gb=32, diag=False, centering=False
-    ):
+    def __init__(self, device=torch.device('cpu'), mem_gb=8, diag=False, centering=False, reg=1e-3):
         super().__init__()
         self.M = None
         self.M_init_scheme = M_init_scheme # specifies how to initialize M
@@ -23,6 +28,8 @@ class RecursiveFeatureMachine(torch.nn.Module):
         self.centering = centering  # if True, update_M will center the gradients before taking an outer product
         self.device = device
         self.mem_gb = mem_gb
+        self.reg = reg # only used when fit using direct solve
+        
 
     def get_data(self, data_loader, batches=None):
         X, y = [], []
@@ -51,41 +58,25 @@ class RecursiveFeatureMachine(torch.nn.Module):
             if self.diag:
                 self.M = torch.ones(centers.shape[-1], device=self.device)
             else:
-                d = centers.shape[-1]
-                if self.M_init_scheme == 'identity':
-                    self.M = torch.eye(d, device=self.device)
-                elif self.M_init_scheme == 'random':
-                    self.M = torch.normal(mean=0.0, std=1/np.sqrt(d), size=(d, d), device=self.device)
-                else:
-                    raise ValueError(f"{self.M_init_scheme} is not valid. Options include `identity` and `random`.")
-        if (len(centers) > 20_000) or self.fit_using_eigenpro:
-            self.weights = self.fit_predictor_eigenpro_old(centers, targets, **kwargs)
+                self.M = torch.eye(centers.shape[-1], device=self.device)
+        if self.fit_using_eigenpro and EIGENPRO_AVAILABLE:
+            self.weights = self.fit_predictor_eigenpro(centers, targets, **kwargs)
         else:
             self.weights = self.fit_predictor_lstsq(centers, targets)
 
     def fit_predictor_lstsq(self, centers, targets):
         return torch.linalg.solve(
-            self.kernel(centers, centers)
-            + 1e-3 * torch.eye(len(centers), device=centers.device),
-            targets,
+            self.kernel(centers, centers) 
+            + self.reg*torch.eye(len(centers), device=centers.device), 
+            targets
         )
 
-    def fit_predictor_eigenpro_old(self, centers, targets, **kwargs):
-        n_classes = 1 if targets.dim() == 1 else targets.shape[-1]
-        self.model = KernelModel(self.kernel, centers, n_classes, verbose=False)
+
+    def fit_predictor_eigenpro(self, centers, targets, **kwargs):
+        n_classes = 1 if targets.dim()==1 else targets.shape[-1]
+        self.model = KernelModel(self.kernel, centers, n_classes, device=self.device)
         _ = self.model.fit(centers, targets, mem_gb=self.mem_gb, **kwargs)
         return self.model.weight
-
-    def fit_predictor_eigenpro(self, centers, targets, test_loader, **kwargs):
-        """Uses new eigenpro3 to train kernel."""
-        n_classes = 1 if targets.dim() == 1 else targets.shape[-1]
-        self.model = KernelModel(
-            n_classes, centers, self.kernel, X=centers, y=targets, devices=[self.device]
-        )
-        _ = self.model.fit(
-            self.model.train_loaders, test_loader, score_fn=accuracy, epochs=10
-        )
-        return self.model.weights
 
     def predict(self, samples):
         print("Number of nan weights:", torch.isnan(self.weights).sum())
@@ -109,11 +100,11 @@ class RecursiveFeatureMachine(torch.nn.Module):
         #     raise NotImplementedError(
         #         "EigenPro method is not yet supported. "+
         #         "Please try again with `method='lstlq'`")
-        self.fit_using_eigenpro = method.lower() == "eigenpro"
-        # self.fit_using_eigenpro = False
+        self.fit_using_eigenpro = (method.lower()=='eigenpro')
+            # self.fit_using_eigenpro = True
 
         train_mses, test_mses = [], []
-
+        
         if loader:
             print("Loaders provided")
             X_train, y_train = self.get_data(train_loader, batches=1)
@@ -121,6 +112,7 @@ class RecursiveFeatureMachine(torch.nn.Module):
         else:
             X_train, y_train = train_loader
             X_test, y_test = test_loader
+
         # X_train, y_train = train_loader
         # X_test, y_test = self.get_data(test_loader, batches=5)
         print("X_train shape:", X_train.shape)
@@ -130,11 +122,10 @@ class RecursiveFeatureMachine(torch.nn.Module):
 
         for i in range(iters):
             self.fit_predictor(X_train, y_train, x_val=X_test, y_val=y_test, **kwargs)
-
             if classif:
                 train_acc = self.score(X_train, y_train, metric="accuracy")
                 print(f"Round {i}, Train Acc: {train_acc:.2f}%")
-                test_acc = self.score(X_test, y_test, metric="accuracy")
+                test_acc = self.score(X_test, y_test, metric='accuracy')
                 print(f"Round {i}, Test Acc: {test_acc:.2f}%")
 
             train_mse = self.score(X_train, y_train, metric="mse")
@@ -158,23 +149,60 @@ class RecursiveFeatureMachine(torch.nn.Module):
             self.update_M(X_train)
 
             if name is not None:
-                hickle.dump(M, f"saved_Ms/M_{name}_{i}.h")
-
-        self.fit_predictor(X_train, y_train)
-        final_mse = self.score(X_test, y_test, metric="mse")
+                hickle.dump(self.M, f"saved_Ms/M_{name}_{i}.h")
+                
+        self.fit_predictor(X_train, y_train, **kwargs)
+        final_mse = self.score(X_test, y_test, metric='mse')
         print(f"Final MSE: {final_mse:.4f}")
         if classif:
             final_test_acc = self.score(X_test, y_test, metric="accuracy")
             print(f"Final Test Acc: {final_test_acc:.2f}%")
+            
+        return train_mses, test_mses
+    
+    def fit_M(self, samples, labels, M_batch_size=None, **kwargs):
+        """Applies EGOP to update the Mahalanobis matrix M."""
+        
+        n, d = samples.shape
+        M = torch.zeros_like(self.M) if self.M is not None else (
+            torch.zeros(d, dtype=samples.dtype) if self.diag else torch.zeros(d, d, dtype=samples.dtype))
+        
+        if M_batch_size is None: # calculate optimal batch size for batched EGOP
+            curr_mem_use = torch.cuda.memory_allocated() # in bytes
+            BYTES_PER_SCALAR = 8
+            p, d = samples.shape
+            c = labels.shape[-1]
+            M_mem = (d if self.diag else d**2)
+            centers_mem = (p * d)
+            mem_available = (self.mem_gb *1024**3) - curr_mem_use - (M_mem + centers_mem + 2*p*d) * BYTES_PER_SCALAR
+            # maximum batch size limited by K, dist, centers_term, samples_term, and G
+            M_batch_size = mem_available // ((2*d + 2*p + 3*c*d + 2)*BYTES_PER_SCALAR)
+        
+        batches = torch.randperm(n).split(M_batch_size)
+        for i, bids in tenumerate(batches):
+            M += self.update_M(samples[bids])
+            
+        self.M = M / n
+        del M
+
+        if self.centering:
+            self.M = self.M - self.M.mean(0)
 
         return train_mses, test_mses
 
     def score(self, samples, targets, metric="mse"):
         preds = self.predict(samples)
-        if metric == "accuracy":
-            # TODO: replace with torchmetrics
-            return (1.0 * (targets == (preds >= 0.5))).mean() * 100.0
-        elif metric == "mse":
+        if metric=='accuracy':
+            if preds.shape[-1]==1:
+                num_classes = len(torch.unique(preds))
+                if num_classes==2:
+                    return accuracy(preds, targets, task="binary").item()
+                else:
+                    return accuracy(preds, targets, task="multiclass", num_classes=num_classes).item()
+            else:
+                return accuracy(preds, targets, task="multilabel", num_labels=preds.shape[-1]).item()
+        
+        elif metric=='mse':
             return (targets - preds).pow(2).mean()
 
 
@@ -182,11 +210,9 @@ class LaplaceRFM(RecursiveFeatureMachine):
     def __init__(self, bandwidth=1.0, **kwargs):
         super().__init__(**kwargs)
         self.bandwidth = bandwidth
-        self.kernel = lambda x, z: laplacian_M(
-            x, z, self.M, self.bandwidth
-        )  # must take 3 arguments (x, z, M)
-
-    def _update_M_batch(self, samples):
+        self.kernel = lambda x, z: laplacian_M(x, z, self.M, self.bandwidth) # must take 3 arguments (x, z, M)
+    
+    def update_M(self, samples):
         """Performs a batched update of M."""
         K = self.kernel(samples, self.centers)
 
@@ -219,7 +245,7 @@ class LaplaceRFM(RecursiveFeatureMachine):
             samples_term = samples_term * (samples * self.M).reshape(n, 1, d)
 
         else:
-            G = (
+            centers_term = (
                 K  # (n, p)
                 @ (
                     self.weights.view(p, c, 1) * (self.centers @ self.M).view(p, 1, d)
@@ -232,13 +258,83 @@ class LaplaceRFM(RecursiveFeatureMachine):
 
             samples_term = samples_term * (samples @ self.M).reshape(n, 1, d)
 
-        G = (G - samples_term) / self.bandwidth  # (n, c, d)
+        G = (centers_term - samples_term) / self.bandwidth  # (n, c, d)
+
+        del centers_term, samples_term, K, dist
         
         # return quantity to be added to M. Division by len(samples) will be done in parent function.
-        return torch.einsum("ncd, ncD -> dD", G, G)
-
-    def update_M(self, samples, batch_size=1000, verbose=False):
         if self.diag:
+            return torch.einsum('ncd, ncd -> d', G, G)
+        else:
+            return torch.einsum("ncd, ncD -> dD", G, G)
+
+class GaussRFM(RecursiveFeatureMachine):
+
+    def __init__(self, bandwidth=1., **kwargs):
+        super().__init__(**kwargs)
+        self.bandwidth = bandwidth
+        self.kernel = lambda x, z: gaussian_M(x, z, self.M, self.bandwidth) # must take 3 arguments (x, z, M)
+        
+
+    def update_M(self, samples):
+        
+        K = self.kernel(samples, self.centers)
+
+        p, d = self.centers.shape
+        p, c = self.weights.shape
+        n, d = samples.shape
+
+        samples_term = (K @ self.weights).reshape(n, c, 1)  # (n, p)  # (p, c)
+
+        if self.diag:
+            centers_term = (
+                K  # (n, p)
+                @ (
+                    self.weights.view(p, c, 1) * (self.centers * self.M).view(p, 1, d)
+                ).reshape(
+                    p, c * d
+                )  # (p, cd)
+            ).view(
+                n, c, d
+            )  # (n, c, d)
+
+            samples_term = samples_term * (samples * self.M).reshape(n, 1, d)
+
+        else:
+            centers_term = (
+                K  # (n, p)
+                @ (
+                    self.weights.view(p, c, 1) * (self.centers @ self.M).view(p, 1, d)
+                ).reshape(
+                    p, c * d
+                )  # (p, cd)
+            ).view(
+                n, c, d
+            )  # (n, c, d)
+
+            samples_term = samples_term * (samples @ self.M).reshape(n, 1, d)
+
+        G = (centers_term - samples_term) / self.bandwidth**2 # (n, c, d)
+        
+        if self.centering:
+            G = G - G.mean(0) # (n, c, d)
+        
+        # return quantity to be added to M. Division by len(samples) will be done in parent function.
+        if self.diag:
+            self.M = torch.einsum('ncd, ncd -> d', G, G)/len(samples)
+        else:
+            self.M = torch.einsum('ncd, ncD -> dD', G, G)/len(samples)
+
+if __name__ == "__main__":
+    torch.set_default_dtype(torch.float32)
+    torch.manual_seed(0)
+    # define target function
+    def fstar(X):
+        return torch.cat([
+            (X[:, 0]  > 0)[:,None],
+            (X[:, 1]  < 0.1)[:,None]],
+            axis=1).type(X.type())
+
             raise NotImplementedError("Diagonal LaplaceRFM not implemented yet.")
         
         n = len(samples)
@@ -277,14 +373,18 @@ if __name__ == "__main__":
     # create low rank data
     n = 4000
     d = 100
-    np.random.seed(0)
-    X_train = torch.from_numpy(np.random.normal(scale=0.5, size=(n, d)))
-    X_test = torch.from_numpy(np.random.normal(scale=0.5, size=(n, d)))
-
-    y_train = fstar(X_train).double()
-    y_test = fstar(X_test).double()
+    torch.manual_seed(0)
+    X_train = torch.randn(n,d)
+    X_test = torch.randn(n,d)
+    
+    y_train = fstar(X_train)
+    y_test = fstar(X_test)
 
     model = LaplaceRFM(bandwidth=1.0, diag=False, centering=False)
     model.fit(
-        (X_train, y_train), (X_test, y_test), loader=False, iters=5, classif=False
+        (X_train, y_train), 
+        (X_test, y_test), 
+        loader=False, method='eigenpro', epochs=15, print_every=5,
+        iters=5,
+        classif=False
     )
